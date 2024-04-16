4a5c7936004ad1fde6111e3658b10ccd951b9ad4
Satyam <stkumar0601@gmail.com>
2024-04-16 23:13:06 +0530

    Added the Analytics part

---
+++ Data Analytics/Python/notes.md
@@ -0,0 +1,45 @@
+# PYTHON
+
+- ### It is High Level Language general purpose Programming Language.
+- ### It was created by Guido van Rossum in 1991.
+
+## Data Types
+
+- ### Integer
+
+  - 10 ,13
+  - it ranges from -infinty to + infinity
+- ### Float
+
+  - 1.20 , -34.045 , -23.0
+  - it ranges from -infinty to + infinity but only floating values
+- ### Boolean
+
+  - True
+  - False
+- ### String
+
+  - Anything written in double quotes or tripple quotes or double tripple quotes. Ex: "Satyam" , ''' Rajiv'''
+- ### Complex
+
+  - A+Bj  , where A is the real part and B is the imaginary part
+
+## OPERATORS
+
+```mermaid
+graph TB;
+A[OPERATORS]-->B[ARITHEMATIC OPERATORS]
+A-->C[LOGICAL OPERATORS]
+A-->D[COMPARISION OPERATORS]
+A-->E[BITWISE OPERATORS]
+```
+
+### Arithematic Operators:
+
+- '+' , '-' , '*' , '/' include arithematic operators used for operations like addition , substraction , multiplication , division etc....
+- 2+3 = 5
+- 5-4 = 1
+- 4*9 = 36
+- 9/4 = 2.25
+
+### Logical Operators:
---
+++ Data Analytics/Streamlit/files.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+
+st.subheader('UpLoading CSV Files : ')
+df = st.file_uploader('Upload your CSV file', type = ['csv' , 'xlsx'])
+
+st.subheader('Loading CSV Files : ')
+df = pd.read_csv(r"C:\Users\Satyam\Desktop\Shubham_Notes\Data Science\Data Analytics\Streamlit\Products.csv")
+if df is not None:
+    st.table(df.head(5))
+
+st.subheader('Dealing with Images Directly : ')
+st.image(r"C:\Users\Satyam\Desktop\Shubham_Notes\Data Science\Data Analytics\Streamlit\img.png")
+
+st.subheader('Dealing with Images via Uploading : ')
+
+img_file = st.file_uploader('Upload your image', type = ['jpg' , 'png'])
+
+if img_file is not None:
+    st.image(img_file)
+
+
+st.subheader('Dealing with Videos :')
+
+video_file = st.file_uploader('Upload your video', type = ['mp4' , 'mkv'])
+if video_file is not None:
+    st.video(video_file)
+
+st.subheader('Dealing with Audio :')
+
+audio_file = st.file_uploader('Upload your audio', type = ['mp3' , 'wav'])
+if audio_file is not None:
+    st.audio(audio_file)
